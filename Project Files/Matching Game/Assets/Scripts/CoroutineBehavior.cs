546a3d40c2c02ff7b7756e6f3544ffdb061504c3
ColbyC <10876761@my.uvu.edu>
2022-09-21 19:17:22 -0600

    Finished Matching Game 2 Lab

---
+++ Project Files/Matching Game/Assets/Scripts/CoroutineBehavior.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CoroutineBehavior : MonoBehaviour
+{
+    public bool canRun;
+    public UnityEvent startEvent, startCountEvent, repeatCountEvent, endCountEvent, repeatUntilFalse;
+    public IntData counterNum;
+    private WaitForSeconds wfsObj;
+    public float seconds = 3.0f;
+    private WaitForFixedUpdate wffuObj;
+
+    private void Start()
+    {
+        wfsObj = new WaitForSeconds(seconds);
+        wffuObj = new WaitForFixedUpdate();
+        startEvent.Invoke();
+    }
+
+    public void StartCounting()
+    {
+        StartCoroutine(Counting());
+    }
+    private IEnumerator Counting()
+    {
+        
+        startCountEvent.Invoke();
+        yield return wfsObj;
+        while (counterNum.value > 0)
+        {
+            Debug.Log(counterNum);
+            repeatCountEvent.Invoke();
+            counterNum.value--;
+            yield return wfsObj;
+        }
+        endCountEvent.Invoke();
+    }
+
+    public void StartRepeatUntilFalse()
+    {
+        canRun = true;
+        StartCoroutine(RepeatUntilFalse());
+    }
+    
+    private IEnumerator RepeatUntilFalse()
+    {
+        while (canRun)
+        {
+            yield return wfsObj;
+            repeatUntilFalse.Invoke();
+        }
+
+    }
+    
+}
--- Project Files/Matching Game/Assets/Scripts/SpriteBehavior.cs
+++ Project Files/Matching Game/Assets/Scripts/SpriteBehavior.cs
@@ -16,4 +16,10 @@ public class SpriteBehavior : MonoBehaviour
     {
         rendererObj.color = obj.value;
     }
+
+    public void ChangeRendererColor(ColorIDDataList obj)
+    {
+        rendererObj.color = obj.currentColor.value;
+
+    }
 }
